c34e2ef1f26aed4936829d974db2a6f7790f8ffa
ErikaHuszar <erikahuszar8@gmail.com>
2020-05-01 20:22:21 +0100

    Testing

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -10,6 +10,6 @@ export const mapServices = [
     name: 'Mapbox',
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    url: `https://api.mapbox.com/styles/v1/erika888/ck9oh5sua03s81ile0db0z5dd.html?fresh=true&title=view&access_token=pk.eyJ1IjoiZXJpa2E4ODgiLCJhIjoiY2s5b2g1cDlmMDBqeDNpbjFmOG81ZmtmZiJ9.CyCMr-vtpQlx6hjQOmuotA`
+    url: `https://api.mapbox.com/styles/v1/erika888/ck9oh5sua03s81ile0db0z5dd/tiles/{256}/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZXJpa2E4ODgiLCJhIjoiY2s5b2t2amkzMDFvdjNncG5kMDgxOWliOSJ9.8FL8wdzKgw7RHjArDH4fIw`
   }
 ];
