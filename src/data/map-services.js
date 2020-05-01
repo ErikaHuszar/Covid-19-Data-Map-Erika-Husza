f41ba78cd9ded99f866ec1871930edcaec08e4df
ErikaHuszar <erikahuszar8@gmail.com>
2020-05-01 20:37:28 +0100

    Changed attribution

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -8,8 +8,7 @@ export const mapServices = [
   },
   {
     name: 'Mapbox',
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    url: `https://api.mapbox.com/styles/v1/erika888/ck9oh5sua03s81ile0db0z5dd/tiles/{256}/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZXJpa2E4ODgiLCJhIjoiY2s5b2t2amkzMDFvdjNncG5kMDgxOWliOSJ9.8FL8wdzKgw7RHjArDH4fIw`
+    attribution: '<a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+    url: 'https://api.mapbox.com/styles/v1/erika888/ck9oh5sua03s81ile0db0z5dd/tiles/{256}/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZXJpa2E4ODgiLCJhIjoiY2s5b2t2amkzMDFvdjNncG5kMDgxOWliOSJ9.8FL8wdzKgw7RHjArDH4fIw'
   }
 ];
