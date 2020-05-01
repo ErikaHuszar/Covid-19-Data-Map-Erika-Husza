a1b40c8d95217ff8061405989fc1a85fda23166b
ErikaHuszar <erikahuszar8@gmail.com>
2020-05-01 20:03:55 +0100

    access token

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -10,6 +10,6 @@ export const mapServices = [
     name: 'Mapbox',
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    url: `https://api.mapbox.com/styles/v1/erika888/ck9oh5sua03s81ile0db0z5dd.html?fresh=true&title=view&access_token=${accessToken}`
+    url: `https://api.mapbox.com/styles/v1/erika888/ck9oh5sua03s81ile0db0z5dd.html?fresh=true&title=view&access_token=pk.eyJ1IjoiZXJpa2E4ODgiLCJhIjoiY2s5b2g1cDlmMDBqeDNpbjFmOG81ZmtmZiJ9.CyCMr-vtpQlx6hjQOmuotA`
   }
 ];
